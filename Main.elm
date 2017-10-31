81cb044abe72a871e859295400aa96e8edf7edb6
aikchun <aikchun616@gmail.com>
2017-10-31 10:39:02 +0800

    type signatures and records

--- Main.elm
+++ Main.elm
@@ -5,13 +5,33 @@ import Html exposing (text)
 
 ask : String -> String -> String
 
-ask thing place = "is there a " ++ thing ++ " in the " ++ place ++ "?"
+ask thing place =
+    "is there a " ++ thing ++ " in the " ++ place ++ "?"
 
 politely : String -> String
 
-politely phrase = "Excuse me, " ++ phrase
+politely phrase =
+    "Excuse me, " ++ phrase
 
 askPolitelyAboutFish : String -> String
 askPolitelyAboutFish = politely << (ask "fish")
-main = text 
-        <| askPolitelyAboutFish "house"
+
+type alias Dog =
+    { name : String
+    , age : Int
+    }
+
+dog = 
+    { name = "Spock"
+    , age = 23
+    }
+
+renderDog : Dog -> String
+
+renderDog dog =
+    dog.name ++ ", " ++ toString dog.age
+
+
+main = 
+    text 
+    <| renderDog dog

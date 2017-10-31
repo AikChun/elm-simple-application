4040a8bd6631b331fc57056f77a6eafc91490763
aikchun <aikchun616@gmail.com>
2017-10-31 13:28:16 +0800

    Able to reduce a list of records

--- Main.elm
+++ Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+import List
+
 
 ask : String -> String -> String
 
@@ -32,6 +34,36 @@ renderDog dog =
     dog.name ++ ", " ++ toString dog.age
 
 
+type alias Person = 
+    { name : String
+    , age : Int
+    }
+
+foo = ["one", "two", "three"]
+
+people = 
+    [ { name = "Legolas", age = 2931 }
+    , { name = "Gimli", age = 193 }
+    ]
+
+names: List Person -> List String
+
+names peeps =
+    List.map (\peep -> peep.name) peeps
+
+findPerson : String -> List Person -> Maybe Person
+
+findPerson name peeps =
+    List.foldl
+        (\peep memo -> case memo of
+            Just _  -> memo
+            Nothing -> if ( peep.name == name ) then
+                Just peep
+                else Nothing
+        ) Nothing peeps
+        
+
 main = 
     text 
-    <| renderDog dog
+    <| toString
+    <| findPerson "Legolas" people
